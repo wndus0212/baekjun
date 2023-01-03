07e8719b33b68849ef125795213ce8c1a3f23d81
wndus <beny020101@gmail.com>
2023-01-03 12:01:38 +0900

    asdf

--- baekjun math 2/src/ex1/PrimeNumber.java
+++ baekjun math 2/src/ex1/PrimeNumber.java
@@ -1,5 +1,28 @@
 package ex1;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class PrimeNumber {
-
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int n=s.nextInt();
+		int answer=0;
+		for(int i=0; i<n; i++) {
+			boolean isPrime=true;
+			int number= s.nextInt();
+			int sqr= (int) Math.sqrt(number);
+			
+			for(int j=2; j<=sqr;j++) {
+				if(number%j==0) {
+					isPrime=false;
+					break;
+				}
+			}
+			if((isPrime&&number!=1)||number==2) {
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
+	}
 }
